c9a9b9713d05508993b93d59d3b99810cff63366
Unknown <rpadmanabhan@scu.edu>
2017-12-02 17:39:09 -0800

    Login works
    before_action :authorize is not enabled

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,31 @@
 Rails.application.routes.draw do
+  root "shopper#index", as: 'root'
+
+  # post 'access/new', to:'access#new'
+  # post 'access', to:'access#new'
+
+  post 'access/new', to:'access#create'
+
+  get 'access/new', to:'access#new'
+
+  get 'login', to: 'access#new'
+
+  get 'access/create'
+  get 'authenticate', to: 'access#create'
+
+  get 'access/destroy'
+  get 'logout', to: 'access#destroy'
+
+  delete 'logout', to: 'access#destroy'
+
+  get 'admin/index'
+  get 'admin', to: 'admin#index'
+
+  resources :users
   resources :orders
   resources :lineitems
   resources :carts
-root "shopper#index", as: 'root'
+
   get 'shopper/index'
   get 'shopper', to: 'shopper#index'
 
